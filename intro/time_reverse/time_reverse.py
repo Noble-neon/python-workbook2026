965fa9fd8e46a4b5f5410d467561220385d6e84f
Noble-neon <muminovsadullo777@gmail.com>
2026-09-04 20:06:30 +0500

    time_reverse

---
+++ intro/time_reverse/time_reverse.py
@@ -0,0 +1,12 @@
+"""186330
+2:03:45:30"""
+sec = int(input())
+days = sec // (60**2*24)
+sec -= days * 60**2*24
+hours = sec //(60**2)
+sec -= hours * 60**2
+minutes = sec // 60
+sec -= minutes * 60
+
+
+print(f"{days}:{hours:02d}:{minutes:02d}:{sec:02d}")
