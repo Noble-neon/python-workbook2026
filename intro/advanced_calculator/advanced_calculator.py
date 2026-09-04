871bb3d20a25b958844995e745a6fc3f1225c2e7
Noble-neon <muminovsadullo777@gmail.com>
2026-09-04 11:37:36 +0500

    advanced calculator

---
+++ intro/advanced_calculator/advanced_calculator.py
@@ -0,0 +1,17 @@
+a = int(input("Input your first number"))
+b = int(input("input your second number"))
+
+"""15 + 4 = 19
+15 - 4 = 11
+15 * 4 = 60
+15 / 4 = 3.75
+15 // 4 = 3
+15 % 4 = 3
+15 ** 4 = 50625"""
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
+print(f"{a} // {b} = {a // b}")
+print(f"{a} % {b} = {a % b}")
+print(f"{a} ** {b} = {a ** b}")
