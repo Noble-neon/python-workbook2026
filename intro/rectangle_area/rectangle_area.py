c212515b9390590255f603ea7fa8322709ddf738
Noble-neon <muminovsadullo777@gmail.com>
2026-09-04 17:18:16 +0500

    rectangle_area.py rectangle_perimeter.py

---
+++ intro/rectangle_area/rectangle_area.py
@@ -0,0 +1,5 @@
+length = float(input("length"))
+width = float(input("width"))
+Area =  length * width
+
+print(f"{Area:.2f}")
---
+++ intro/rectangle_perimeter/rectangle_perimeter.py
@@ -0,0 +1,5 @@
+length = float(input("length"))
+width = float(input("width"))
+Area =  (length + width) * 2
+
+print(f"{Area:.2f}")
