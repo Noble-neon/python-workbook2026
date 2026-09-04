447d29366209d80753b3bc31cb021ee25ca4b4fc
Noble-neon <muminovsadullo777@gmail.com>
2026-09-04 17:58:05 +0500

    temperature

---
+++ intro/temperature/temperature.py
@@ -0,0 +1,8 @@
+"""Fahrenheit = (Celsius × 9/5) + 32
+Temperature in Fahrenheit: 212.00"""
+
+Celsius = int(input())
+
+Fahrenheit = (Celsius * 9 / 5) + 32
+
+print(f"Temperature in Fahrenheit: {Fahrenheit:.2f}")
